5812af5375bec56f29851c28ff85fe0d8b6b5afb
Nicolas Schmitt <scalpmail@gmail.com>
2016-04-07 16:49:50 +0200

    change deployment

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var uglify      = require("gulp-uglify");
 var rename      = require("gulp-rename");
 var sass        = require("gulp-sass");
 var cssnano     = require("gulp-cssnano");
-var babel       = require('gulp-babel');
+var babel       = require("gulp-babel");
+var usemin      = require('gulp-usemin');
 
 // Auto
 
@@ -24,19 +25,7 @@ gulp.task("watch-sass", function() {
    gulp.watch('client/css/*.scss', ['sass']); 
 });
 
-gulp.task("app-js", function() {
-    return gulp.src(["client/app/app.js", "client/app/scripts/*.js"])
-               .pipe(concat("myapp.min.js"))
-               .pipe(babel({ presets: ['es2015'] }).on("error", gutil.log))
-               .pipe(uglify().on("error", gutil.log))
-               .pipe(gulp.dest("client/app"));
-});
-
-gulp.task("watch-js", function() {
-   gulp.watch(["client/app/app.js", 'client/app/scripts/*.js'], ['app-js']); 
-});
-
-gulp.task("watch", [ "watch-sass", "watch-js" ]);
+gulp.task("watch", [ "watch-sass" ]);
 
 // Dev
 
@@ -68,7 +57,21 @@ gulp.task('bower-min-css', function() {
                .pipe(gulp.dest("client/lib"));
 });
 
-
 // Main tasks
 
-gulp.task('default', ["clean", 'bower-js', 'bower-min-js', 'bower-css', 'bower-min-css']);
+gulp.task('default', [ "clean", 'bower-js', 'bower-min-js', 'bower-css', 'bower-min-css', "sass" ]);
+
+// Prod
+
+gulp.task("replace-html", function() {
+    return gulp.src("client/index.html")
+                .pipe(usemin({
+                    js: [
+                        babel({ presets: ['es2015'] }),
+                        uglify()
+                    ]
+                }))
+                .pipe(gulp.dest("client/"));
+});
+
+gulp.task("prod", [ "sass", "replace-html" ]);
